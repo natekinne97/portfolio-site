e406112ec391f78dd856f715da9e5d3379c6781e
Nate Kinne <nathan.kinne@gmail.com>
2019-06-01 12:59:26 -0500

    some javascript

---
+++ code.js
@@ -0,0 +1,73 @@
+// mobile section
+// this will handle all the button inputs on 
+// the mobile screen
+
+// shows the menu items when burger is clicked
+function showMenu(){
+    $(document).on('click', '.fa-bars', function(){
+        styleMenuAndItems();
+    });
+}
+
+//change style of menu items for click
+function styleMenuAndItems(){
+    console.log('styling items');
+    // change navbar to 100% height for better view
+    $('nav').toggleClass('mobile-click');
+    // resize the imaege
+    $('.top-bar img').toggleClass('img-clicked-style');
+    // display links
+    $('nav ul').toggleClass('nav-links-mobile');
+}
+
+
+// links clicked
+function linksClicked(){
+    $(document).on('click', '.nav-links a', function(){
+        styleMenuAndItems();
+    });
+}
+
+// Everything for mobile functioning
+function mobileFunctions(){
+    showMenu();
+    linksClicked();
+}
+
+// Desktop side. highlight menu items on scroll. 
+function checkSectionVisible(){
+    // get nav links and find corresponding sections
+    let menu = $('.nav-links'),
+        // list of all items
+        menuItems = menu.find('a'),
+        // anchors to menu items
+        scrollItems = menuItems.map(function () {
+            let item = $($(this).attr('href'));
+            if(item.length){return item;}
+        })
+        console.log(scrollItems);
+        console.log(menu);
+        console.log(menu.length);
+    $(window).scroll(function(){
+        // get conainter position
+        let fromTop = $(this).scrollTop();
+
+        // get id of current scroll item
+        let cur = scrollItems.map(function(){
+            if($(this).offset().top < fromTop)
+                return this;
+        });
+
+        // get current element
+        cur = cur[cur.length-1];
+        let id = cur && cur.length ? cur[0].id : "";
+        console.log(id);
+        console.log(cur);
+
+    });
+}
+
+(function(){
+mobileFunctions();
+checkSectionVisible();
+}());
